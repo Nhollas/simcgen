73d446f630ffddafc8e4f5ef99cd46a462082f5e
Nick Hollas <nicholashollas@yahoo.co.uk>
2023-12-03 13:12:45 +0000

    Add prettier config

--- src/lib/wowhead.ts
+++ src/lib/wowhead.ts
@@ -1,31 +1,33 @@
 function formatItemName(name?: string) {
-    if (!name) {
-      return "";
-    }
+  if (!name) {
+    return "";
+  }
 
-    return name.toLowerCase().replace(/'/g, "").replace(/ /g, "-");
+  return name.toLowerCase().replace(/'/g, "").replace(/ /g, "-");
+}
+
+function formatBonusId(bonusId?: string) {
+  if (!bonusId) {
+    return "";
   }
-  
-  function formatBonusId(bonusId?: string) {
-    if (!bonusId) {
-      return "";
-    }
-  
-    return bonusId.split("/").join(":");
-  }
+
+  return bonusId.split("/").join(":");
+}
 
 export function createTooltipUrl(item: any, setPieces?: number[]) {
-    return `https://www.wowhead.com/item=${item.id}/${formatItemName(
-      item.name
-    )}?bonus=${formatBonusId(item.bonus_id)}&ilvl=${
-      item.itemLevel
-    }&ench=${item.enchant_id}&gems=${item.socketInfo?.PRISMATIC?.gemIds.join(":")}&spec=103${item.itemSetId ? "&pcs=" + setPieces?.join(":") : ""}`;
-  }
+  return `https://www.wowhead.com/item=${item.id}/${formatItemName(
+    item.name
+  )}?bonus=${formatBonusId(item.bonus_id)}&ilvl=${item.itemLevel}&ench=${
+    item.enchant_id
+  }&gems=${item.socketInfo?.PRISMATIC?.gemIds.join(":")}&spec=103${
+    item.itemSetId ? "&pcs=" + setPieces?.join(":") : ""
+  }`;
+}
 
 export function createGemTooltipUrl(gem: any) {
-  return `https://www.wowhead.com/item=${gem.itemId}?crafting-quality=6`
+  return `https://www.wowhead.com/item=${gem.itemId}?crafting-quality=6`;
 }
 
 export function createEnchantTooltipUrl(enchant: any) {
-  return `https://www.wowhead.com/item=${enchant.itemId}?crafting-quality=6`
+  return `https://www.wowhead.com/item=${enchant.itemId}?crafting-quality=6`;
 }
